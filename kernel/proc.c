4cb91ae01aae03c646535bf36d8a02a15bc387bd
Jeremy <brom.eeyhib@gmail.com>
2023-04-09 11:09:45 -0400

    Add mapping for usyscall

--- kernel/proc.c
+++ kernel/proc.c
@@ -202,6 +202,16 @@ proc_pagetable(struct proc *p)
     return 0;
   }
 
+  // Map a page for the usyscall struct.
+  if (mappages(pagetable, USYSCALL, PGSIZE,
+              (uint64)(p->usyscall), PTE_R) < 0)
+  {
+    uvmunmap(pagetable, TRAMPOLINE, 1, 0);
+    uvmunmap(pagetable, TRAPFRAME, 1, 0);
+    uvmfree(pagetable, 0);
+    return 0;
+  }
+
   return pagetable;
 }
 
@@ -212,6 +222,7 @@ proc_freepagetable(pagetable_t pagetable, uint64 sz)
 {
   uvmunmap(pagetable, TRAMPOLINE, 1, 0);
   uvmunmap(pagetable, TRAPFRAME, 1, 0);
+  uvmunmap(pagetable, USYSCALL, 1, 0);
   uvmfree(pagetable, sz);
 }
 
